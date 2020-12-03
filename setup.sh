d963f1a8237c76cec9f74319f116acdfe3fb1453
Fabian Neumann <fabian.neumann@outlook.de>
2020-12-03 20:26:46 +0100

    remove technology data submodule

--- setup.sh
+++
@@ -1,5 +0,0 @@
-cp config.technology-data.yaml subworkflows/technology-data/config.yaml
-cd subworkflows/technology-data
-rm -rf outputs
-snakemake -j 1 -f compile_cost_assumptions
-cp outputs/costs_2050.csv ../pypsa-eur/resources/costs.csv
